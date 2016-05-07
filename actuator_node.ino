f9d32daed960560a52acbd65cbb4811dca8f57a2
jmyth742 <jmyth742@users.noreply.github.com>
2016-05-07 14:44:59 +0100

    Update actuator_node.ino

--- actuator_node.ino
+++ actuator_node.ino
@@ -3,6 +3,7 @@
 
 #define LIGHTS_RELAY 8
 #define PUMP_RELAY 9
+#define SUB_PUMP 7
 #define ACTUATOR_ADDRESS 6
 #define FAN_CONTROL 2
 
@@ -11,12 +12,14 @@ void pumpCallback();
 void lightsCallback();
 void fanCallback();
 
+//global variables
+
 bool lights_state = false;
 bool pump_state = false;
+float t[2]={};
 
-
-Task t_pump(4000, TASK_FOREVER, &pumpCallback);
-Task t_lights(1000, TASK_FOREVER, &lightsCallback);
+Task t_pump(15000, TASK_FOREVER, &pumpCallback);
+Task t_lights(12000000, TASK_FOREVER, &lightsCallback);
 
 Scheduler runner;
 
@@ -30,7 +33,7 @@ void setup() {
 
    runner.init();
   Serial.println("Scheduler started");
-
+  //ADD THE TASKS   
   runner.addTask(t_pump);
   runner.addTask(t_lights);
 
@@ -61,15 +64,29 @@ void pumpCallback() {
 
 
 void receiveEvent(int howMany) {
-  int x = Wire.read();    // receive byte as an integer
-  Serial.println(x); 
-  analogWrite(FAN_CONTROL, x); 
+  //this function also acts as the message handler for the I2C communication
+  int i;  
+  while (Wire.available()) { 
+  t[i]= Wire.read();    // receive byte as an integer
+  i=i+1; 
+  }
+  //message for the temp controller
+  if(t[0] == 1){
+    int x= t[1];
+    analogWrite(FAN_CONTROL, x);
+  }
+  //message for the EC controller 
+  else if(t[0] == 2){
+    int y =t[1];
+    digitalWrite(SUB_PUMP, HIGH);
+    
+  }
 }
 
 void lightsCallback() {
   //this function controls the LED lights
   //turning it on and off at a predefined interval to mimic sun light
-  Serial.println("fdgtg");
+  Serial.println("light call back function");
       if (lights_state == false) {
         digitalWrite(LIGHTS_RELAY, HIGH);
       lights_state = true;
