5084152518d2bdc8c9aefb97b68d25ea3c1bf36e
jmyth742 <jmyth742@users.noreply.github.com>
2016-05-07 14:45:17 +0100

    Update sensor_node.ino

--- sensor_node.ino
+++ sensor_node.ino
@@ -1,35 +1,75 @@
 #include <Wire.h>
+#include "DHT.h"
 
-const int temperaturePin = 0;
+
+#define DHTPIN 2     // what digital pin we're connected to
+#define DHTTYPE DHT11   // DHT 11
+
+#define ECPIN 0
+
+//global variables
+float ec;
+int v;
+float current = 0.04;
+int temp_max = 45;
+int ec_max = 2;
+int humid_max = 75;
 
 #define SENSOR_ADDRESS 8
 
+DHT dht(DHTPIN, DHTTYPE);//instance of DHT class instantiated
+
 void setup() {
   Serial.begin(9600);
+  Serial.println("begin sensor read");
+   dht.begin();
   Wire.begin(SENSOR_ADDRESS);                // join i2c bus with address #8
   Wire.onRequest(requestEvent); // register event
 }
 
 void loop() {
   delay(5000);
-  Serial.println(get_temp());
+  Serial.println(get_temp());//used for debugging
 }
 
 float get_temp(){
-   float voltage, degreesC;
-  voltage = getVoltage(temperaturePin);
-   degreesC = (((voltage - 0.5) * 100.0) /8);
-   return degreesC;
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  if(t>temp_max){
+    t=45;
+  }
+   return t;
+}
+
+float get_ec(){
+  v = analogRead(ECPIN);
+  //convert the value to voltage
+  v = v*(5.0/1023);// this gives us the current voltage across the probe
+  //now we need to get the inverse of the resistance which is siemens
+  ec = current/v;
+  if(ec>ec_max){
+    ec=2;
+  }
+  return ec;
+}
+
+float get_humidity(){
+  float h = dht.readHumidity();
+  if(h > humid_max){
+    h = 75;
+  }
+  return h;
 }
 
 
-float getVoltage(int pin){
-  return (analogRead(pin) * 0.004882814);
-}
 
 // function that executes whenever data is requested by master
 // this function is registered as an event, see setup()
 void requestEvent() {
   int t = get_temp();
+  int h = get_humidity();
+  int ec = get_ec();
   Wire.write(t);  // as expected by master
+  Wire.write(h);
+  Wire.write(ec);
 }
